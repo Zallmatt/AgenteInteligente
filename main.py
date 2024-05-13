b2ad9fddba82e2bc2282b12e2bb315588e1c2460
Zallmatt <matizalazar2001@gmail.com>
2024-05-13 18:37:53 -0300

    Arreglos del diccionario

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from nltk.tokenize import word_tokenize
 preguntas_y_respuestas = {
     "difultades Dificultades difucltades Difucltades": "Las dificultades en la gestión de los legajos internos incluyen limitaciones de escalabilidad, problemas de seguridad y rendimiento en la base de datos. Además, parte de la gestión se realiza a través de Onedrive, donde se almacenan imágenes de algunas características de los presos.",
     
-    "base de datos, Base de Datos, actualmente" : "Actualmente, la base de datos es un Access y un OneDrive en donde se almacenan las imagenes de los presos",
+    "base datos, Base, actualmente" : "Actualmente, la base de datos es un Access y un OneDrive en donde se almacenan las imagenes de los presos",
     
     "informacion gestion gestiona actualmente": """La información que se gestiona actualmente es:
                                                     *Carpeta médica
@@ -21,7 +21,7 @@ preguntas_y_respuestas = {
                                                     *Senias particulares
                                                     """,
     
-    "actualizaciones cada cuanto": "Todos los días se realizan actualizaciones de datos por la contínua entrada y salida de condenados y procesados.",
+    "actualizaciones actualiza cada cuanto": "Todos los días se realizan actualizaciones de datos por la contínua entrada y salida de condenados y procesados.",
     
     "Seguridad seguridad confidencialidad medidas": "Actualmente unicamente pueden tener acceso los empleados que cumplan con los roles requeridos.",
     
@@ -35,8 +35,9 @@ preguntas_y_respuestas = {
     Cada área cuenta con disponibilidad de computadoras y cada uno se encarga de cargar los datos pertinentes a cada area
     Los datos obligatorios van a depender de cada área.
     """,
-    "tecnologia tiene cada area, Con que tecnologia cuenta cada area, de que se encarga cada area":"Cada área cuenta con disponibilidad de computadoras y cada uno se encarga de cargar los datos pertinentes a su area",
-    "funcionalidades específicas esperan obtener  sistema de gestión  legajos  internos? requisitos cumplir las funcionalidades de una base de datos ": 
+    
+    "elementos, disponibilidad, dispositivo, dispositivos":"Cada área cuenta con disponibilidad de computadoras y cada uno se encarga de cargar los datos pertinentes a su area",
+    "funcionalidades, requerimientos, requerimiento": 
     """Los mismos con lo que ya cuentan actualmente, que son: Visualización, Consulta.
     Se requiere que se agreguen alertas en casos específicos.
     También la base debe tener una actualización diaria, en lo posible, debido a que tienen muchos ingresos y egresos diarios.
@@ -48,7 +49,7 @@ preguntas_y_respuestas = {
     *Tiempo de beneficio
     *Fase de liberación""",
     
-    "codigo/codigo de acceso para agregar descripciones?":"Cada sector tiene un codigo de acceso y debe estar habilitado para agregar descripciones""",
+    "codigo acceso para agregar descripciones":"Cada sector tiene un codigo de acceso y debe estar habilitado para agregar descripciones""",
     
     "niveles ": "Se tienen 4 niveles: Judiciales, Educación, Sanidad, Trabajo",
     
@@ -60,27 +61,27 @@ preguntas_y_respuestas = {
                 *Trabajo
                 Cada sector tiene un codigo de acceso y debe estar habilitado para agregar descripciones
                 """,
-    " implementación  impacto impactos base de datos impacte eficiencia procesos gestión penitenciaria carcel servicio penitenciario? espera luego base datos implementación impacte eficiencia procesos cree o proceso carga lectura datos Como sería el proceso de carga y lectura de datos?": """Se espera obtener la Carta de situación
+    " se espera, impacto, idea, procesos": """Se espera obtener la Carta de situación
     La idea es que se vinculen datos o crucen entre internos. Los vinculos a tener en cuenta son: 
     * Relaciones entre internos.
     * Organigrama de visitas.
     * Alerta de persona ya registrada en visitas.
     * Cruzaba información entre internos. Esta utlima ya esta creada pero es obsoleta.
     """,
-    "¿supervisa el progreso del proyecto y asegura que los entregables se completen a tiempo y dentro del presupuesto?":" El  Gestor de Proyecto es Luis Gonzalez",
-    "¿Quienes participan   proyecto? Cuales son los participantes del trabajo? cuales son los participantes del proyecto? Quienes quienes participan proyecto":"Los alumnos de 4to y 5to año de la carrera de Ingenieria en Sistemas de la Informacion de la Universidad de la Cuenca del Plata",
-    "¿Cual duracion  proyecto? Duracion duracion proyecto": "El planeamiento se llevara a cabo durante el primer cuatrimestre a cargo de los alumnos de 5to año y el desarrollo de la base de datos se llevara a cabo en el segundo cuatrimestre a cargo de los alumnos de 4to año",
-    " podes dar  resumen  contexto del proyecto?Contexto Resumen resumen proyecto":"La implementación de una base de datos para la gestión de legajos de internos en todas las dependencias del Servicio Penitenciario permite una gestión más eficiente de la información y contribuye a la construcción de ciudades y comunidades sostenibles.",
-    "¿Cual   objetivo  proyecto? Objetivo objetivo proposito":"Implementar una base de datos centralizada y segura para la gestión de los legajos de los internos del Servicio Penitenciario de la Provincia de Corrientes, garantizando el acceso y actualización de la información de manera remota y eficiente.",
-    "¿informacion tenes manejas?  tenes informacion?Podes contarme podes Informacion informacion tenes contame para  fuiste creado?":"""Tengo informacion acerca del proyecto Gestión Integral de Legajos de Internos en el Servicio Penitenciario de la Provincia de Corrientes.Puedo hablarte de los objetivos del proyecto, los participantes, fecha de realizacion y responder preguntas en base a la primera reunion realizada""",
-    "Como estas hoy?estas como ":"Soy un bot, no tengo estado de animo",
-    " puedes hacer?  hacer puedes¿Para  fuiste creado?creado para otra respuesta":"Fui creado para contestar las preguntas que me hagas sobre la implementacion de una base de datos para el Servicio Penitenciario para tu trabajo práctico 2 de tu carrera",
-    "¿Que sos?, sos un agente inteligente?":"Si soy un agente inteligente",
-    "Gracias amigo gracias muchas gracias":"De nada, ojala te recibas pronto, recorda que el titulo intermedio tambien vale",
-    "Que nota crees  merecemos? nota merecemos merecer calificacion":"En mi opinion de agente inteligente, mi voto es secreto",
-    "Dale nota merecemos?dale":"Un 10, Felicidades!!!!!",
-    "¿Cual nombre? Tenes nombre tienes nombre cual como te llamas":"Mi nombre es Tobi, no te confundas con un perro soy un agente inteligente", 
-    "Contame  chiste Chiste contas chistes? tenes chistes para contar?" :"El programador se quedó atascado en la ducha debido a las instrucciones del estado del champú:Hacer espuma, enjuagar, repetir",
+    "Lider, lider, coordinador, supervisor":" El  Gestor de Proyecto es Luis Gonzalez",
+    "participantes, participante, alumnos, alumno, integrantes, integrante, carrera":"Los alumnos de 4to y 5to año de la carrera de Ingenieria en Sistemas de la Informacion de la Universidad de la Cuenca del Plata",
+    "duracion, tiempo ejecucion": "El planeamiento se llevara a cabo durante el primer cuatrimestre a cargo de los alumnos de 5to año y el desarrollo de la base de datos se llevara a cabo en el segundo cuatrimestre a cargo de los alumnos de 4to año",
+    "contexto, resumen":"La implementación de una base de datos para la gestión de legajos de internos en todas las dependencias del Servicio Penitenciario permite una gestión más eficiente de la información y contribuye a la construcción de ciudades y comunidades sostenibles.",
+    "Objetivo, objetivo, proposito, Propositos":"Implementar una base de datos centralizada y segura para la gestión de los legajos de los internos del Servicio Penitenciario de la Provincia de Corrientes, garantizando el acceso y actualización de la información de manera remota y eficiente.",
+    "tenes, funcion, finalidad":"""Tengo informacion acerca del proyecto Gestión Integral de Legajos de Internos en el Servicio Penitenciario de la Provincia de Corrientes.Puedo hablarte de los objetivos del proyecto, los participantes, fecha de realizacion y responder preguntas en base a la primera reunion realizada""",
+    "Como estas como ":"Soy un bot, no tengo estado de animo",
+    "creado":"Fui creado para contestar las preguntas que me hagas sobre la implementacion de una base de datos para el Servicio Penitenciario para tu trabajo práctico 2 de tu carrera",
+    "sos, agente inteligente consideras":"Si me considero un agente inteligente",
+    "Gracias":"De nada, ojala te recibas pronto, recorda que el titulo intermedio tambien vale",
+    "notas":"En mi opinion de agente inteligente, mi voto es secreto",
+    "Dale nota merecemos dale":"Un 10, Felicidades!!!!!",
+    "nombre, llamas":"Mi nombre es Tobi, no te confundas con un perro soy un agente inteligente", 
+    "chiste" :"El programador se quedó atascado en la ducha debido a las instrucciones del estado del champú:Hacer espuma, enjuagar, repetir",
     "Contame otro chiste otro Otro":"¿Qué es un terapeuta? 1024 Gigapeutas.",           
 }
 # Tokeniza las preguntas y respuestas
